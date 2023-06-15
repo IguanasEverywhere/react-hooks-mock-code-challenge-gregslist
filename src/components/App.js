5a6db86479e2755da337a11530c67377e90dd176
Scott Schwab <scottschwab86@gmail.com>
2023-06-15 17:17:01 -0500

    complete core deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,13 @@ import ListingsContainer from "./ListingsContainer";
 
 function App() {
 
+  const [allListings, setAllListings] = useState([]);
   const [listingsToDisplay, setListingsToDisplay] = useState([]);
-
-  console.log('listingsToDisplay is', listingsToDisplay)
-
-  const allListings = listingsToDisplay;
 
   function removeItemFromListing(id) {
     const listingsAfterDelete = listingsToDisplay.filter((listing) => listing.id !== id);
     setListingsToDisplay(listingsAfterDelete);
+    setAllListings(allListings.filter((listing) => listing.id !== id));
   }
 
   function filterSearchResults(searchQuery) {
@@ -23,12 +21,16 @@ function App() {
   useEffect(() => {
     fetch('http://localhost:6001/listings')
       .then(r => r.json())
-      .then(listings => setListingsToDisplay(listings))
+      .then(listings => {
+        setListingsToDisplay(listings);
+        setAllListings(listings);
+      }
+      )
   }, [])
 
   return (
     <div className="app">
-      <Header filterSearchResults={filterSearchResults}/>
+      <Header filterSearchResults={filterSearchResults} />
       <ListingsContainer
         listingsToDisplay={listingsToDisplay}
         removeItemFromListing={removeItemFromListing}
